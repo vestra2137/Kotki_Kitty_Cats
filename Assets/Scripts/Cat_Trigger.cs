6f18b62262c4c86137b8fd7c532c21e88da582ae
aga <167030698+Kokoszambo@users.noreply.github.com>
2025-12-02 16:25:11 +0100

    skrypty
    zrobione: migające światła,
    zaczęte: tekst zawsze obraca się w stronę kamery,  pole aktywacji kota (pojawia sie jego imie)

---
+++ Assets/Scripts/Cat_Trigger.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using TMPro;
+
+public class Cat_Trigger : MonoBehaviour
+{
+    public TextMeshProUGUI catText;
+    private void OnTriggerEnter(Collider other)
+    {
+        
+    }
+}
--- Assets/Scripts/Flickering_light.cs
+++ Assets/Scripts/Flickering_light.cs
@@ -1,6 +1,24 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
-public class Flickering_light
+public class Flickering_light : MonoBehaviour
 {
-    
+    private Light light;
+    public float minIntensity = .5f;
+    public float maxIntensity = 5.0f;
+    public float flickerSpeed = 0.1f;
+
+    private void Start()
+    {
+        light = GetComponent<Light>();
+
+        InvokeRepeating("Flicker", 0f, flickerSpeed);
+        
+    }
+    private void Flicker()
+    {
+        float randomIntensity = Random.Range(minIntensity, maxIntensity);
+        light.intensity = randomIntensity;  
+    }
+
 }
