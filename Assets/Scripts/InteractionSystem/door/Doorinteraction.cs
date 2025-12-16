188a72f2b7762c0410783ae648272b163f1d139a
Skop <filipp328@gmail.com>
2025-12-16 16:49:02 +0100

    Interact base

--- Assets/Scripts/InteractionSystem/door/Doorinteraction.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-public class Doorinteraction : MonoBehaviour
-{
-    
-        
-    //otwieranie dzrwi za pomoca klucza: animacja otwieranai drzwi, odpala sie tylko wtedy gdy bohater uzyje klucza, warunek posiadania przedmiotu, jak zefinowac posiadanie przedmiotu, czy jest eq
-}
