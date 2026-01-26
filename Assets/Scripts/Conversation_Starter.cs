2c47733a417aaed86f4bc425bcf40455f5a3f952
Skop <filipp328@gmail.com>
2026-01-26 18:37:35 +0100

    Dialogi Final

--- Assets/Scripts/Conversation_Starter.cs
+++ Assets/Scripts/Conversation_Starter.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using DialogueEditor;
 
-public class Conversation_Starter : MonoBehaviour
+public class ConversationStarter : MonoBehaviour
 {
     [SerializeField] private NPCConversation DialogueTest;
     private void OnTriggerStay(Collider other)
